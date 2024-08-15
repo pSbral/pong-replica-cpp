1f95368740bcd674a076399519fc6b7cff13394f
mrcat <80040598+pSbral@users.noreply.github.com>
2024-08-15 18:42:44 -0300

    Rendering Progression

---
+++ renderer.cpp
@@ -0,0 +1,8 @@
+void render_background() {
+	unsigned int* pixel = (unsigned int*)render_state.memory;
+	for (int y = 0; y < render_state.height; y++) {
+		for (int x = 0; x < render_state.width; x++) {
+			*pixel++ = x * y;
+		}
+	}
+}
--- win32_plataform.cpp
+++ win32_plataform.cpp
@@ -2,6 +2,18 @@
 
 bool window_exec = true;
 
+struct RenderState
+{
+	int height, width;
+	void* memory;
+
+	BITMAPINFO bitmap_info;
+};
+
+RenderState render_state;
+
+#include "renderer.cpp"
+
 LRESULT window_callback(
 	HWND    hWnd,
 	UINT    Msg,
@@ -14,7 +26,27 @@ LRESULT window_callback(
 		case WM_CLOSE:
 		case WM_DESTROY: {
 			window_exec = false;
-		}
+		} break;
+		case WM_SIZE: {
+			RECT rect;
+			GetClientRect(hWnd, &rect);
+
+			render_state.width = rect.right - rect.left;
+			render_state.height = rect.bottom - rect.top;
+
+			int size = render_state.width * render_state.height * sizeof(unsigned int);
+
+			if (render_state.height) VirtualFree(render_state.memory, 0, MEM_RELEASE);
+			render_state.memory = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+			render_state.bitmap_info.bmiHeader.biSize = sizeof(render_state.bitmap_info.bmiHeader);
+			render_state.bitmap_info.bmiHeader.biWidth = render_state.width;
+			render_state.bitmap_info.bmiHeader.biHeight = render_state.height;
+			render_state.bitmap_info.bmiHeader.biPlanes = 1;
+			render_state.bitmap_info.bmiHeader.biBitCount = 32;
+			render_state.bitmap_info.bmiHeader.biCompression = BI_RGB;
+
+		} break;
 		default: {
 			result = DefWindowProc(hWnd, Msg, wParam, lParam);
 		}
@@ -53,6 +85,7 @@ int WinMain(
 		1280, 720, 
 		0, 0, 
 		hInstance, 0);
+	HDC hdc = GetDC(window);
 
 	if (!window) {
 		MessageBox(NULL, L"Falha ao criar a janela.", L"Erro", MB_OK | MB_ICONERROR);
@@ -61,11 +94,18 @@ int WinMain(
 
 	// Loop de mensagens
 	while (window_exec) {
+		//Input
 		MSG msg = {};
 		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
+
+		//Simulate
+		render_background();
+
+		//Render
+		StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory, &render_state.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
 	}
 
 	return 0;
