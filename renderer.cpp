9a5cf7346348c0e08955e4e4e86957a28e1f8a70
mrcat <80040598+pSbral@users.noreply.github.com>
2024-08-20 06:22:56 -0300

    Renderer Progression 3

--- renderer.cpp
+++ renderer.cpp
@@ -2,7 +2,7 @@ void render_background() {
 	unsigned int* pixel = (unsigned int*)render_state.memory;
 	for (int y = 0; y < render_state.height; y++) {
 		for (int x = 0; x < render_state.width; x++) {
-			*pixel++ = x * y;
+			*pixel++ = x + y *2;
 		}
 	}
 }
