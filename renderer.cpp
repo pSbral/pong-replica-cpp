5d834c51322d32677841226a32f8778fe6a4996d
mrcat <80040598+pSbral@users.noreply.github.com>
2024-08-20 08:01:09 -0300

    v 0.0.2

---
+++ renderer.cpp
@@ -0,0 +1,46 @@
+internal void
+clear_screen(u32 color) {
+	unsigned int* pixel = (u32*)render_state.memory;
+	for (int y = 0; y < render_state.height; y++) {
+		for (int x = 0; x < render_state.width; x++) {
+			*pixel++ = color;
+		}
+	}
+}
+
+internal void
+render_rect(int x0, int y0, int x1, int y1, u32 color) {
+
+	x0 = clamp(0, x0, render_state.width);
+	x1 = clamp(0, x1, render_state.width);
+	y0 = clamp(0, y0, render_state.height);
+	y1 = clamp(0, y1, render_state.height);
+
+	for (int y = y0; y < y1; y++) {
+		u32* pixel = (u32*)render_state.memory + x0 + y * render_state.width;
+		for (int x = x0; x < x1; x++) {
+			*pixel++ = color;
+		}
+	}
+}
+
+global_variable float render_scale = 0.01f;
+
+internal void
+draw_rect(float x, float y, float half_size_x, float half_size_y, u32 color) {
+	x *= render_state.height * render_scale;
+	y *= render_state.height * render_scale;
+	half_size_x *= render_state.height * render_scale;
+	half_size_y *= render_state.height * render_scale;
+
+	x += render_state.width / 2.f;
+	y += render_state.height / 2.f;
+
+	// Change to pixels
+	int x0 = x - half_size_x;
+	int x1 = x + half_size_x;
+	int y0 = y - half_size_y;
+	int y1 = y + half_size_y;
+
+	render_rect(x0, y0, x1, y1, color);
+}
--- win32_plataform.cpp
+++ win32_plataform.cpp
@@ -1,8 +1,22 @@
 #include <Windows.h>
+#include "utils.cpp"
 
-bool window_exec = true;
 
-LRESULT window_callback(
+global_variable bool window_exec = true;
+
+struct RenderState
+{
+	int height, width;
+	void* memory;
+
+	BITMAPINFO bitmap_info;
+};
+
+global_variable RenderState render_state;
+
+#include "renderer.cpp"
+
+LRESULT CALLBACK window_callback(
 	HWND    hWnd,
 	UINT    Msg,
 	WPARAM  wParam,
@@ -14,7 +28,27 @@ LRESULT window_callback(
 		case WM_CLOSE:
 		case WM_DESTROY: {
 			window_exec = false;
-		}
+		} break;
+		case WM_SIZE: {
+			RECT rect;
+			GetClientRect(hWnd, &rect);
+
+			render_state.width = rect.right - rect.left;
+			render_state.height = rect.bottom - rect.top;
+
+			int size = render_state.width * render_state.height * sizeof(u32);
+
+			if (render_state.height) VirtualFree(render_state.memory, 0, MEM_RELEASE); // Liberar memória antiga, se existir
+			render_state.memory = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+			render_state.bitmap_info.bmiHeader.biSize = sizeof(render_state.bitmap_info.bmiHeader);
+			render_state.bitmap_info.bmiHeader.biWidth = render_state.width;
+			render_state.bitmap_info.bmiHeader.biHeight = render_state.height; // Valor negativo inverte os eixos verticais
+			render_state.bitmap_info.bmiHeader.biPlanes = 1;
+			render_state.bitmap_info.bmiHeader.biBitCount = 32;
+			render_state.bitmap_info.bmiHeader.biCompression = BI_RGB;
+
+		} break;
 		default: {
 			result = DefWindowProc(hWnd, Msg, wParam, lParam);
 		}
@@ -30,6 +64,9 @@ int WinMain(
 	LPSTR     lpCmdLine,
 	int       nShowCmd
 ) {
+	// Inicializa a memória
+	render_state.memory = NULL;
+
 	//Criando a classe da janela de execução
 	WNDCLASS window_class = {};
 	window_class.style = CS_HREDRAW | CS_VREDRAW;
@@ -52,20 +89,48 @@ int WinMain(
 		CW_USEDEFAULT, CW_USEDEFAULT, 
 		1280, 720, 
 		0, 0, 
-		hInstance, 0);
-
+		hInstance, 0
+	);
+	
 	if (!window) {
 		MessageBox(NULL, L"Falha ao criar a janela.", L"Erro", MB_OK | MB_ICONERROR);
 		return -1;
 	}
 
+	HDC hdc = GetDC(window);
+
 	// Loop de mensagens
 	while (window_exec) {
+		//Input
 		MSG msg = {};
 		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
+			switch (msg.message) {
+				case WM_KEYUP:
+				case WM_KEYDOWN: {
+
+				} break;
+
+				default: {
+					TranslateMessage(&msg);
+					DispatchMessage(&msg);
+				}
+			}
 		}
+
+		//Simulate
+		clear_screen(0xff5500);
+		draw_rect(0, 0, 5, 5, 0x00ff22);
+
+		//Render
+		StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory, &render_state.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
+	}
+
+	// Libera o HDC
+	ReleaseDC(window, hdc);
+
+	// Libera a memória alocada
+	if (render_state.memory) {
+		VirtualFree(render_state.memory, 0, MEM_RELEASE);
 	}
 
 	return 0;
